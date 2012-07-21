0cc89c81c83f43e0708c2d08c08ab8dd4db7e3a7
Gabriel <gabriel.casarini@gmail.com>
2012-07-21 20:53:08 +0200

    minor change

--- test/model/AllTests.java
+++ test/model/AllTests.java
@@ -4,6 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * Run all test cases
+ *
+ */
 @RunWith(Suite.class)
 @SuiteClasses({
 	BoardTestCase.class,
